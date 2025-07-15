c6920874eaf759ec85b097ec42f387cef84ae852
WasayBaig10 <wasaybaig617@gmail.com>
2025-07-15 22:02:12 +0500

    update commit changes

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@
 
 // module.exports = nextConfig
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
